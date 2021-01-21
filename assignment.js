02b29f6706c7e76bb9bbcf31f64ae278057500c8
Naeem Mahmud Al-Amin <naeemmahmud370@gmail.com>
2021-01-22 00:33:51 +0600

    second is done

--- assignment.js
+++ assignment.js
@@ -1,6 +1,15 @@
-function kilometerToMeter(kilometer){
+function kilometerToMeter(kilometer) {
     var meter = kilometer * 1000;
     return meter;
 }
 var result = kilometerToMeter(10);
 console.log(result);
+
+
+
+function budgetCalculator(watch, phone, laptop){
+    var total = watch + phone + laptop;
+    return total;
+}
+var sum = budgetCalculator(50, 100, 500);
+console.log(sum);
